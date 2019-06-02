9eb1de51b02e5829e01543a0ff784f8f5f01272b
Enzo Zheng <cool007zqw@163.com>
2019-06-02 21:55:10 +0800

    vds-fetch-params added, line ending fixed

--- VDS_Install.sh
+++ VDS_Install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-wget http://www.vdsheaven.com/statics/V-Dimension-0.9.13-1-1f520b4-amd64.deb
-apt install ./V-Dimension-0.9.13-1-1f520b4-amd64.deb
+wget http://www.vdsheaven.com/statics/V-Dimension-0.9.13-1-1f520b4-amd64.deb -O VDS.deb
+apt install ./VDS.deb
 mkdir ~/.vds
 wget https://raw.githubusercontent.com/cool007zqw/HandyScripts/master/vds.conf -P ~/.vds
+vds-fetch-params
 vdsd
