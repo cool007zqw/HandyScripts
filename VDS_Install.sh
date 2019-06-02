67bea7f071351d2a56959102aad2c8c8126b643f
Enzo Zheng <cool007zqw@163.com>
2019-06-02 22:12:59 +0800

    fail to fix

--- VDS_Install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-wget http://www.vdsheaven.com/statics/V-Dimension-0.9.13-1-1f520b4-amd64.deb -O VDS.deb
-apt install ./VDS.deb
-mkdir ~/.vds
-wget https://raw.githubusercontent.com/cool007zqw/HandyScripts/master/vds.conf -P ~/.vds
-vds-fetch-params
-vdsd
