06f490ecb735a1797e42d9f29fdb40e0f4e524cb
Enzo Zheng <cool007zqw@163.com>
2019-06-17 19:57:25 +0800

    simplified version

--- ckb_ipv4.sh
+++ ckb_ipv4.sh
@@ -2,27 +2,14 @@
 
 #wget https://github.com/nervosnetwork/ckb/releases/download/v0.14.0/ckb_v0.14.0_x86_64-unknown-linux-gnu.tar.gz
 
-wget https://www.dropbox.com/s/8nctqs1d7vjuk0j/ckb_v0.14.0_x86_64-unknown-linux-gnu.tar.gz?dl=0 -O ckb_v0.14.0_x86_64-unknown-linux-gnu.tar.gz
-tar -xzvf ckb_v0.14.0_x86_64-unknown-linux-gnu.tar.gz
-cd ckb_v0.14.0_x86_64-unknown-linux-gnu
+wget https://www.dropbox.com/s/u31ng82m1tt0h45/ckb.tar.gz -O ckb.tar.gz
+tar -xzvf ckb.tar.gz
+cd ckb
 sudo ln -snf "$(pwd)/ckb" /usr/local/bin/ckb
 
-ckb init -C ckb-testnet --chain testnet
-cd ckb-testnet
 
-
-sed -i 's/threads     = 1/threads     = 3/g' ckb-miner.toml
-sed -i 's/127.0.0.1:8114/hk.enzo.host:8114/g' ckb-miner.toml
-
-cat <<EOT >> ckb.toml
-[block_assembler]
-code_hash = "0xf1951123466e4479842387a66fabfd6b65fc87fd84ae8e6cd3053edb27fff2fd"
-args = ["0x1fa5af45df2c6ba478e9bb06cf6f594b88356b16"]
-EOT
-
-sleep 1
 tmux new-session -d -s "miner"
-sleep 1
-tmux send-keys -t "miner" "ckb miner -C ~/ckb_v0.14.0_x86_64-unknown-linux-gnu/ckb-testnet/" C-m
-sleep 1
+#sleep 1
+tmux send-keys -t "miner" "ckb miner -C ~/ckb/ckb-testnet/" C-m
+#sleep 1
 tmux a
