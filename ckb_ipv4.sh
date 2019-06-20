35f96179c43db2c7653b5432c17eaa0ced5f2214
Enzo Zheng <cool007zqw@163.com>
2019-06-20 20:50:23 +0800

    tmux cd

--- ckb_ipv4.sh
+++ ckb_ipv4.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
 #wget https://github.com/nervosnetwork/ckb/releases/download/v0.14.0/ckb_v0.14.0_x86_64-unknown-linux-gnu.tar.gz
-
 wget https://www.dropbox.com/s/u31ng82m1tt0h45/ckb.tar.gz -O ckb.tar.gz
 tar -xzvf ckb.tar.gz
 cd ckb
 sudo ln -snf "$(pwd)/ckb" /usr/local/bin/ckb
 
-
 tmux new-session -d -s "miner"
-#sleep 1
-tmux send-keys -t "miner" "ckb miner -C ~/ckb/ckb-testnet/" C-m
-#sleep 1
-tmux a
+tmux send-keys -t "miner" "cd ~/ckb/ckb-testnet/" C-m
+tmux send-keys -t "miner" "ckb miner" C-m
